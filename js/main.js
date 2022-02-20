5c75d90726e9172db027a099a9c7dcddbe1a19ec
Khariton90 <82729640+Khariton90@users.noreply.github.com>
2022-02-20 23:17:40 +0300

    Линтинг

--- js/main.js
+++ js/main.js
@@ -1,10 +1,9 @@
 const generateRandomNumber = (min, max) => {
-    let range = max - min;
-    if (range > 0) {
-        return Math.random() * (range + 1) + min;
-    }
-
-    return min >= 0 ? min : 0;
+  const range = max - min;
+  if (range > 0) {
+    return Math.random() * (range + 1) + min;
+  }
+  return min >= 0 ? min : 0;
 
 };
 
@@ -12,5 +11,5 @@ const getRandomInteger = (min, max) => Math.floor(generateRandomNumber(min, max)
 
 const getRandomFloat = (min, max, fraction) => +generateRandomNumber(min, max).toFixed(fraction);
 
-console.log(getRandomInteger(0, 10));
-console.log(getRandomFloat(0, 10, 2));
+getRandomInteger(0, 10);
+getRandomFloat(0, 10, 2);

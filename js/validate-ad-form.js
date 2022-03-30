5d57d404e1b1a3329695feeff1072fda664232f5
Khariton90 <82729640+Khariton90@users.noreply.github.com>
2022-03-30 20:49:27 +0300

    Правда или действие (часть 2)

--- js/validate-ad-form.js
+++ js/validate-ad-form.js
@@ -1,12 +1,13 @@
-import { TITLE_MAX_LENGTH, TITLE_MIN_LENGTH, housePriceTypes } from './data.js';
+import { TITLE_MAX_LENGTH, TITLE_MIN_LENGTH, housePriceTypes, HUNDRED_ROOMS, NOT_GUESTS } from './data.js';
 
 const adForm = document.querySelector('.ad-form');
 const titleField = adForm.querySelector('#title');
 const priceField =  adForm.querySelector('#price');
 const typeSelect = adForm.querySelector('#type');
-const allSelectFields = adForm.querySelectorAll('select');
 const roomsSelect = adForm.querySelector('#room_number');
 const capacitySelect = adForm.querySelector('#capacity');
+const timeFields = adForm.querySelector('.ad-form__element--time');
+const timeSelectGroup = timeFields.querySelectorAll('select');
 
 let minPriceValue = housePriceTypes.house;
 let errorCapacityMessage = '';
@@ -29,29 +30,33 @@ function changePriceMinValue(evt) {
 }
 
 function validatePrice(value) {
-  const minValue = +priceField.placeholder;
+  const minValue = Number(priceField.placeholder);
   return value >= minValue;
 }
 
 function getErrorMessagePrice() {
-  return `Не меньше ${+minPriceValue} руб.`;
+  return `Не меньше ${minPriceValue} руб.`;
 }
 
 function validateMinLength(value) {
   return value.length > TITLE_MIN_LENGTH;
 }
 
-// Костыль) не придумал что то получше
 function getRoomsForGuests(rooms) {
-  const guests = capacitySelect.value;
+  const guests = Number(capacitySelect.value);
+  rooms = Number(rooms);
 
-  if (+rooms === 100) {
+  if (rooms === HUNDRED_ROOMS) {
     errorCapacityMessage = 'Не для гостей';
-    return +guests === 0;
-  } else if (rooms < guests) {
+    return guests === NOT_GUESTS;
+  }
+
+  if (rooms < guests) {
     errorCapacityMessage = `Для ${guests} гостей нужно больше комнат`;
     return false;
-  } else if (+rooms < 100 && +guests === 0) {
+  }
+
+  if (rooms < 100 && guests === NOT_GUESTS) {
     errorCapacityMessage = 'Для гостей';
     return false;
   }
@@ -62,25 +67,30 @@ function getCapacityErrorMessage() {
   return errorCapacityMessage;
 }
 
-pristine.addValidator(roomsSelect, getRoomsForGuests, getCapacityErrorMessage, true);
+function getTitleLengthMessage() {
+  return `От ${TITLE_MIN_LENGTH} до ${TITLE_MAX_LENGTH} символов`;
+}
+
+timeFields.addEventListener('change', (evt) => {
+  timeSelectGroup.forEach((select) => {
+    if (select !== evt.target) {
+      select.value = evt.target.value;
+    }
+  });
+});
+
+
+pristine.addValidator(roomsSelect, getRoomsForGuests, getCapacityErrorMessage);
 
 pristine.addValidator(priceField, validatePrice, getErrorMessagePrice);
 
-pristine.addValidator(titleField, validateMinLength, `От ${TITLE_MIN_LENGTH} до ${TITLE_MAX_LENGTH} символов`);
+pristine.addValidator(titleField, validateMinLength, getTitleLengthMessage);
 
 typeSelect.addEventListener('change', changePriceMinValue);
-
-titleField.addEventListener('input', () => pristine.validate());
-
-allSelectFields.forEach((select) => select.addEventListener('change', () => pristine.validate()));
 
 adForm.addEventListener('submit', (evt) => {
   evt.preventDefault();
   const isValid = pristine.validate();
 
-  if (isValid) {
-    // console.log('валидна');
-  } else {
-    // console.log('невалидна');
-  }
+  return isValid;
 });
